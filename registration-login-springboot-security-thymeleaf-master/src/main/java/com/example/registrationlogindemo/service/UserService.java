d34dd61068f52acf8527fea053fa361702d2c8af
LeQuan0610 <111440475+LeQuan0610@users.noreply.github.com>
2023-04-13 15:05:52 +0700

    Add files via upload

--- registration-login-springboot-security-thymeleaf-master/src/main/java/com/example/registrationlogindemo/service/UserService.java
+++ registration-login-springboot-security-thymeleaf-master/src/main/java/com/example/registrationlogindemo/service/UserService.java
@@ -2,10 +2,13 @@ package com.example.registrationlogindemo.service;
 
 import com.example.registrationlogindemo.dto.UserDto;
 import com.example.registrationlogindemo.entity.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 public interface UserService {
+
     void saveUser(UserDto userDto);
 
     User findByEmail(String email);
